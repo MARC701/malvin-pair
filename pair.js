bf52fbad65268c585dbc2a89ddd19f061df9a3d2
𝘼𝙧𝙨𝙡𝙖𝙣 𝘾𝙝𝙖𝙪𝙙𝙖𝙧𝙮 <143324285+MARC701@users.noreply.github.com>
2025-07-20 14:03:29 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,11 +75,11 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "malvin~" + string_session;
+                        let md = "𝙼𝙰𝙵𝙸𝙰-𝙼𝙳~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
-                        let desc = `*Hey there, MALVIN-XD User!* 👋🏻
+                        let desc = `*Hey there, 𝐌𝐀𝐅𝐈𝐀-𝐌𝐃 User!* 👋🏻
 
-Thanks for using *MALVIN-XD* — your session has been successfully created!
+Thanks for using *𝐌𝐀𝐅𝐈𝐀-𝐌𝐃* — your session has been successfully created!
 
 🔐 *Session ID:* Sent above  
 ⚠️ *Keep it safe!* Do NOT share this ID with anyone.
@@ -88,23 +88,23 @@ Thanks for using *MALVIN-XD* — your session has been successfully created!
 
 *✅ Stay Updated:*  
 Join our official WhatsApp Channel:  
-https://whatsapp.com/channel/0029VbA6MSYJUM2TVOzCSb2A
+https://whatsapp.com/channel/0029Vat4TFC0QeaoLURbP61u
 
-*💻 Source Code:*  
-Fork & explore the project on GitHub:  
-https://github.com/XdKing2/MALVIN-XD
+*💻 Tiktok:*  
+Follow us on Titkok:  
+https://www.tiktok.com/@arslan_chaudary_22?_t=ZS-8yBHkUnkJK3&_r=1
 
 ——————
 
-> *© Powered by Malvin King*
+> *© Powered by 𝘼𝙧𝙨𝙡𝙖𝙣 𝘾𝙝𝙖𝙪𝙙𝙖𝙧𝙮*
 Stay cool and hack smart. ✌🏻`; 
                         await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "ᴍᴀʟᴠɪɴ-xᴅ",
-thumbnailUrl: "https://files.catbox.moe/bqs70b.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029VbA6MSYJUM2TVOzCSb2A",
+title: "𝙼𝙰𝙵𝙸𝙰-𝙼𝙳",
+thumbnailUrl: "https://files.catbox.moe/j3j6pn.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029Vat4TFC0QeaoLURbP61u",
 mediaType: 1,
 renderLargerThumbnail: true
 }  
@@ -113,9 +113,9 @@ renderLargerThumbnail: true
 {quoted:code })
                     } catch (e) {
                             let ddd = sock.sendMessage(sock.user.id, { text: e });
-                            let desc = `Hey there, MALVIN-XD User!* 👋🏻
+                            let desc = `Hey there, 𝐌𝐀𝐅𝐈𝐀-𝐌𝐃 User!* 👋🏻
 
-Thanks for using *MALVIN-XD* — your session has been successfully created!
+Thanks for using *𝙼𝙰𝙵𝙸𝙰-𝚃𝚎𝚌𝚑* — your session has been successfully created!
 
 🔐 *Session ID:* Sent above  
 ⚠️ *Keep it safe!* Do NOT share this ID with anyone.
@@ -124,23 +124,23 @@ Thanks for using *MALVIN-XD* — your session has been successfully created!
 
 *✅ Stay Updated:*  
 Join our official WhatsApp Channel:  
-https://whatsapp.com/channel/0029VbA6MSYJUM2TVOzCSb2A
+https://whatsapp.com/channel/0029Vat4TFC0QeaoLURbP61u
 
 *💻 Source Code:*  
-Fork & explore the project on GitHub:  
-https://github.com/XdKing2/MALVIN-XD
+Follow us on Tiktok:  
+https://www.tiktok.com/@arslan_chaudary_22?_t=ZS-8yBHkUnkJK3&_r=1
 
 ——————
 
-> *© Powered by Malvin King*
+> *© Powered by 𝘼𝙧𝙨𝙡𝙖𝙣 𝘾𝙝𝙖𝙪𝙙𝙖𝙧𝙮*
 Stay cool and hack smart. ✌🏻`;
                             await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "ᴍᴀʟᴠɪɴ-xᴅ",
-thumbnailUrl: "https://i.imgur.com/GVW7aoD.jpeg",
-sourceUrl: "https://whatsapp.com/channel/0029VbA6MSYJUM2TVOzCSb2A",
+title: "𝙼𝙰𝙵𝙸𝙰-𝙼𝙳",
+thumbnailUrl: "https://files.catbox.moe/j3j6pn.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029Vat4TFC0QeaoLURbP61u",
 mediaType: 2,
 renderLargerThumbnail: true,
 showAdAttribution: true
