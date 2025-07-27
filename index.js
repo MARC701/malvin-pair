464936ebd630b602ceda71a08fa75a6008a3ef1a
𝘼𝙧𝙨𝙡𝙖𝙣 𝘾𝙝𝙖𝙪𝙙𝙖𝙧𝙮 <143324285+MARC701@users.noreply.github.com>
2025-07-27 14:50:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forgot To Give Follow MAFIA-TECH
+Don't Forgot To Follow MAFIA-TECH
 
  Server running on http://localhost:` + PORT)
 })
